9c50dcc59fa2b1c34870f89742ae9510e627c2c5
Ufuk Köşker <ufuk.kskr@gmail.com>
2022-12-18 13:16:31 +0300

    refactoring CategoryListRequestModel

--- PlayerArea/PlayerArea/Modules/Main/Models/Requests/CategoryListRequestModel.swift
+++ PlayerArea/PlayerArea/Modules/Main/Models/Requests/CategoryListRequestModel.swift
@@ -8,17 +8,9 @@
 import Foundation
 
 struct CategoryListRequestModel: Requestable {
-    var parameters: [String : Any] = [:]
     var paths: [String]?
     
     init(paths: [String]) {
         self.paths = paths
-        prepareRequest()
-    }
-    
-    mutating func prepareRequest() {
-        self.parameters = [
-            "key": "6b4fab3e88ec48aab19933b2c153c7c9"
-        ]
     }
 }
