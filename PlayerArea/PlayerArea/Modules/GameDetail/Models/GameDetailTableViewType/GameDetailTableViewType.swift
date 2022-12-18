342bf45d814c5219d0bf36d9ad4578b8b09039fd
Ufuk Köşker <ufuk.kskr@gmail.com>
2022-12-18 13:06:53 +0300

    created GameDetailTableViewType enum.

---
+++ PlayerArea/PlayerArea/Modules/GameDetail/Models/GameDetailTableViewType/GameDetailTableViewType.swift
@@ -0,0 +1,16 @@
+//
+//  GameDetailTableViewType.swift
+//  PlayerArea
+//
+//  Created by Ufuk Köşker on 15.12.2022.
+//
+
+import Foundation
+
+enum GameDetailTableViewType: Int, CaseIterable {
+    case header = 0
+    case genres
+    case description
+    case platforms
+    case stores
+}
